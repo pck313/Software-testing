038e2db231f944782e3c5d11ea27b6ee4c47f659
pck313 <23021596@vnu.edu.vn>
2026-05-22 15:18:08 +0700

    bug

--- CodeRabbit/LRU/algorithm_buggy.py
+++ CodeRabbit/LRU/algorithm_buggy.py
@@ -5,14 +5,16 @@ def lru_page_replacement(pages, capacity):
     # lưu thời điểm sử dụng gần nhất
     last_used = {}
 
-    page_faults = 0
+    page_faults = 1
     history = []
 
     for time, page in enumerate(pages):
 
-        replaced_index = -1 # chỉ số của page bị thay thế
+        # chỉ số của page bị thay thế
+        replaced_index = -1 
 
-        if page is frames:
+
+        if page not in frames:
 
             status = "Ko lỗi"
 
@@ -20,10 +22,10 @@ def lru_page_replacement(pages, capacity):
 
             status = "Lỗi"
 
-            # page_faults += 1
+            page_faults += 1
 
             # còn chỗ trống
-            if -1 in frames:
+            if 0 in frames:
 
                 replaced_index = frames.index(-1)
 
@@ -32,16 +34,16 @@ def lru_page_replacement(pages, capacity):
                 # tìm page ít dùng gần đây nhất
                 lru_page = min(
                     frames,
-                    key=lambda p: last_used[p]
+                    key=lambda p: last_used[p, 0]
                 )
 
                 replaced_index = frames.index(lru_page)
 
             # thay page
-            frames[replaced_index] = page
+            frames[0] = page
 
         # cập nhật thời gian dùng gần nhất
-        last_used[page] = 0
+        last_used[page] = time + 1
 
         history.append({
             "page": page,
